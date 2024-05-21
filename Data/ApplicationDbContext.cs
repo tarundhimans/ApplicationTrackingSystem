3a55d5ca612aa7bdb8a8f5c8454fc2a0d8163cd9
tarundhimans <dhiman.cssolutions@gmail.com>
2024-05-21 12:47:47 +0530

    startup

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationTrackingSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationTrackingSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ApplicationUser>ApplicationUsers { get; set; }
+
+    }
+}
---
+++ SD.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplicationTrackingSystem
+{
+    public static class SD
+    {
+        public const string Role_Admin = "Admin";
+        public const string Role_CEO = "CEO";
+        public const string Role_HR = "HR";
+        public const string Role_User = "User";
+    }
+}
